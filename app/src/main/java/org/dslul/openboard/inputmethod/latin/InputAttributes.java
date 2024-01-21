leinss/TobiBoard

commit 50a2e9ecc8c05bc9ea10cadd84748d06aa91faf6

only ignore TYPE_TEXT_FLAG_NO_SUGGESTIONS when user explicitly wants it

app/src/main/java/org/dslul/openboard/inputmethod/latin/InputAttributes.java
@@ -89,7 +89,7 @@ public final class InputAttributes {
 
         // TODO: Have a helper method in InputTypeUtils
         // Make sure that passwords are not displayed in {@link SuggestionStripView}.
-        mShouldShowSuggestions = !mIsPasswordField;
+        mShouldShowSuggestions = !mIsPasswordField && !flagNoSuggestions;
 
         mShouldInsertSpacesAutomatically = InputTypeUtils.isAutoSpaceFriendlyType(inputType);
 

app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/SettingsValues.java
@@ -192,8 +192,8 @@ public class SettingsValues {
                 && prefs.getBoolean(Settings.PREF_GESTURE_FLOATING_PREVIEW_TEXT, true);
         mAutoCorrectionEnabledPerUserSettings = mAutoCorrectEnabled;
                 //&& !mInputAttributes.mInputTypeNoAutoCorrect; // follow that request or not?
-        mSuggestionsEnabledPerUserSettings = !mInputAttributes.mIsPasswordField &&
-                readSuggestionsEnabled(prefs);
+        mSuggestionsEnabledPerUserSettings = (mInputAttributes.mShouldShowSuggestions && readSuggestionsEnabled(prefs))
+                || (!mInputAttributes.mIsPasswordField && readSuggestionsOverrideEnabled(prefs));
         mIncognitoModeEnabled = Settings.readAlwaysIncognitoMode(prefs) || mInputAttributes.mNoLearning
                 || mInputAttributes.mIsPasswordField;
         mKeyboardHeightScale = prefs.getFloat(Settings.PREF_KEYBOARD_HEIGHT_SCALE, DEFAULT_SIZE_SCALE);
@@ -252,8 +252,7 @@ public class SettingsValues {
     }
 
     public boolean needsToLookupSuggestions() {
-        return mInputAttributes.mShouldShowSuggestions
-                && (mAutoCorrectionEnabledPerUserSettings || isSuggestionsEnabledPerUserSettings());
+        return mAutoCorrectionEnabledPerUserSettings || isSuggestionsEnabledPerUserSettings();
     }
 
     public boolean isSuggestionsEnabledPerUserSettings() {
@@ -310,6 +309,10 @@ public class SettingsValues {
 
     private static boolean readSuggestionsEnabled(final SharedPreferences prefs) {
         return prefs.getBoolean(Settings.PREF_SHOW_SUGGESTIONS, true);
+    }
+
+    private static boolean readSuggestionsOverrideEnabled(final SharedPreferences prefs) {
+        return prefs.getBoolean(Settings.PREF_ALWAYS_SHOW_SUGGESTIONS, false);
     }
 
     private static boolean readBigramPredictionEnabled(final SharedPreferences prefs,

app/src/main/java/org/dslul/openboard/inputmethod/latin/inputlogic/InputLogic.java
@@ -844,7 +844,7 @@ public final class InputLogic {
                 || mWordComposer.isComposingWord() // emoji will be part of the word in this case, better do nothing
                 || !settingsValues.mBigramPredictionEnabled // this is only for next word suggestions, so they need to be enabled
                 || settingsValues.mIncognitoModeEnabled
-                || !settingsValues.mInputAttributes.mShouldShowSuggestions // see comment in performAdditionToUserHistoryDictionary
+                || !settingsValues.isSuggestionsEnabledPerUserSettings() // see comment in performAdditionToUserHistoryDictionary
                 || !StringUtilsKt.isEmoji(text)
         ) return;
         if (mConnection.hasSlowInputConnection()) {
@@ -1524,8 +1524,8 @@ public final class InputLogic {
         // If correction is not enabled, we don't add words to the user history dictionary.
         // That's to avoid unintended additions in some sensitive fields, or fields that
         // expect to receive non-words.
-        // mInputTypeNoAutoCorrect changed to !mShouldShowSuggestions because this was cancelling learning way too often
-        if (!settingsValues.mInputAttributes.mShouldShowSuggestions || settingsValues.mIncognitoModeEnabled || TextUtils.isEmpty(suggestion))
+        // mInputTypeNoAutoCorrect changed to !isSuggestionsEnabledPerUserSettings because this was cancelling learning way too often
+        if (!settingsValues.isSuggestionsEnabledPerUserSettings() || settingsValues.mIncognitoModeEnabled || TextUtils.isEmpty(suggestion))
             return;
         final boolean wasAutoCapitalized = mWordComposer.wasAutoCapitalized() && !mWordComposer.isMostlyCaps();
         final String word = stripWordSeparatorsFromEnd(suggestion, settingsValues);

app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/CorrectionSettingsFragment.java
@@ -29,20 +29,6 @@ import org.dslul.openboard.inputmethod.latin.userdictionary.UserDictionarySettin
 
 import java.util.TreeSet;
 
-/**
- * "Text correction" settings sub screen.
- *
- * This settings sub screen handles the following text correction preferences.
- * - Personal dictionary
- * - Add-on dictionaries
- * - Block offensive words
- * - Auto-correction
- * - Auto-correction confidence
- * - Show correction suggestions
- * - Personalized suggestions
- * - Suggest Contact names
- * - Next-word suggestions
- */
 public final class CorrectionSettingsFragment extends SubScreenFragment
     implements SharedPreferences.OnSharedPreferenceChangeListener,
         PermissionsManager.PermissionsResultCallback {
@@ -86,6 +72,8 @@ public final class CorrectionSettingsFragment extends SubScreenFragment
                     .setPositiveButton(android.R.string.ok, null)
                     .setOnCancelListener(dialogInterface -> ((TwoStatePreference) findPreference(key)).setChecked(true))
                     .show();
+        } else if (Settings.PREF_SHOW_SUGGESTIONS.equals(key) && !prefs.getBoolean(key, true)) {
+            ((TwoStatePreference)findPreference(Settings.PREF_ALWAYS_SHOW_SUGGESTIONS)).setChecked(false);
         }
         refreshEnabledSettings();
     }
@@ -106,9 +94,9 @@ public final class CorrectionSettingsFragment extends SubScreenFragment
     }
 
     private void refreshEnabledSettings() {
-        setPreferenceVisible(Settings.PREF_AUTO_CORRECTION_CONFIDENCE,
-                Settings.readAutoCorrectEnabled(getSharedPreferences()));
+        setPreferenceVisible(Settings.PREF_AUTO_CORRECTION_CONFIDENCE, Settings.readAutoCorrectEnabled(getSharedPreferences()));
         setPreferenceVisible(Settings.PREF_ADD_TO_PERSONAL_DICTIONARY, getSharedPreferences().getBoolean(Settings.PREF_KEY_USE_PERSONALIZED_DICTS, true));
+        setPreferenceVisible(Settings.PREF_ALWAYS_SHOW_SUGGESTIONS, getSharedPreferences().getBoolean(Settings.PREF_SHOW_SUGGESTIONS, true));
         turnOffLookupContactsIfNoPermission();
     }
 

app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/Settings.java
@@ -83,6 +83,7 @@ public final class Settings implements SharedPreferences.OnSharedPreferenceChang
     public static final String PREF_AUTO_CORRECTION = "pref_key_auto_correction";
     public static final String PREF_AUTO_CORRECTION_CONFIDENCE = "pref_key_auto_correction_confidence";
     public static final String PREF_SHOW_SUGGESTIONS = "show_suggestions";
+    public static final String PREF_ALWAYS_SHOW_SUGGESTIONS = "pref_always_show_suggestions";
     public static final String PREF_KEY_USE_PERSONALIZED_DICTS = "pref_key_use_personalized_dicts";
     public static final String PREF_KEY_USE_DOUBLE_SPACE_PERIOD = "pref_key_use_double_space_period";
     public static final String PREF_BLOCK_POTENTIALLY_OFFENSIVE = "pref_key_block_potentially_offensive";

app/src/main/res/values/strings.xml
@@ -101,6 +101,10 @@
     <string name="prefs_show_suggestions">Show correction suggestions</string>
     <!-- Description for show suggestions -->
     <string name="prefs_show_suggestions_summary">Display suggested words while typing</string>
+    <!-- Option to override app flag to not show suggestions -->
+    <string name="prefs_always_show_suggestions">Always show suggestions</string>
+    <!-- Description for override app flag to not show suggestions -->
+    <string name="prefs_always_show_suggestions_summary">Ignore other apps’ request to disable suggestions</string>
     <!-- Option to block potentially offensive words to be shown [CHAR_LIMIT=30] -->
     <string name="prefs_block_potentially_offensive_title">Block offensive words</string>
     <!-- Summary for option to block potentially offensive words to be shown [CHAR_LIMIT=80 (two lines) or 40 (fits on one line, preferable)] -->

app/src/main/res/xml/prefs_screen_correction.xml
@@ -72,6 +72,13 @@
             android:persistent="true" />
 
         <SwitchPreferenceCompat
+            android:key="pref_always_show_suggestions"
+            android:title="@string/prefs_always_show_suggestions"
+            android:summary="@string/prefs_always_show_suggestions_summary"
+            android:defaultValue="false"
+            android:persistent="true" />
+
+        <SwitchPreferenceCompat
             android:key="pref_key_use_personalized_dicts"
             android:title="@string/use_personalized_dicts"
             android:summary="@string/use_personalized_dicts_summary"

commit 987e66913d9e7b2e87d81f20a33c23e7a235ebad

fix issues with looking up suggestions when it should not happen was introduced in 50a2e9ecc8c05bc9ea10cadd84748d06aa91faf6

app/src/main/java/org/dslul/openboard/inputmethod/latin/InputAttributes.java
@@ -30,6 +30,7 @@ public final class InputAttributes {
     final public boolean mInputTypeNoAutoCorrect;
     final public boolean mIsPasswordField;
     final public boolean mShouldShowSuggestions;
+    final public boolean mMayOverrideShowingSuggestions;
     final public boolean mApplicationSpecifiedCompletionOn;
     final public boolean mShouldInsertSpacesAutomatically;
     final public boolean mShouldShowVoiceInputKey;
@@ -71,6 +72,7 @@ public final class InputAttributes {
                         + " imeOptions=0x%08x", inputType, editorInfo.imeOptions));
             }
             mShouldShowSuggestions = false;
+            mMayOverrideShowingSuggestions = false;
             mInputTypeNoAutoCorrect = false;
             mApplicationSpecifiedCompletionOn = false;
             mShouldInsertSpacesAutomatically = false;
@@ -90,6 +92,7 @@ public final class InputAttributes {
         // TODO: Have a helper method in InputTypeUtils
         // Make sure that passwords are not displayed in {@link SuggestionStripView}.
         mShouldShowSuggestions = !mIsPasswordField && !flagNoSuggestions;
+        mMayOverrideShowingSuggestions = !mIsPasswordField;
 
         mShouldInsertSpacesAutomatically = InputTypeUtils.isAutoSpaceFriendlyType(inputType);
 

app/src/main/java/org/dslul/openboard/inputmethod/latin/settings/SettingsValues.java
@@ -118,6 +118,7 @@ public class SettingsValues {
     public final int mScoreLimitForAutocorrect;
     public final boolean mAutoCorrectionEnabledPerUserSettings;
     private final boolean mSuggestionsEnabledPerUserSettings;
+    private final boolean mOverrideShowingSuggestions;
     public final SettingsValuesForSuggestion mSettingsValuesForSuggestion;
     public final boolean mIncognitoModeEnabled;
     private final AsyncResultHolder<AppWorkaroundsUtils> mAppWorkarounds;
@@ -188,8 +189,9 @@ public class SettingsValues {
                 && prefs.getBoolean(Settings.PREF_GESTURE_FLOATING_PREVIEW_TEXT, true);
         mAutoCorrectionEnabledPerUserSettings = mAutoCorrectEnabled;
                 //&& !mInputAttributes.mInputTypeNoAutoCorrect; // follow that request or not?
+        mOverrideShowingSuggestions = mInputAttributes.mMayOverrideShowingSuggestions && readSuggestionsOverrideEnabled(prefs);
         mSuggestionsEnabledPerUserSettings = (mInputAttributes.mShouldShowSuggestions && readSuggestionsEnabled(prefs))
-                || (!mInputAttributes.mIsPasswordField && readSuggestionsOverrideEnabled(prefs));
+                || mOverrideShowingSuggestions;
         mIncognitoModeEnabled = Settings.readAlwaysIncognitoMode(prefs) || mInputAttributes.mNoLearning
                 || mInputAttributes.mIsPasswordField;
         mKeyboardHeightScale = prefs.getFloat(Settings.PREF_KEYBOARD_HEIGHT_SCALE, DEFAULT_SIZE_SCALE);
@@ -248,7 +250,8 @@ public class SettingsValues {
     }
 
     public boolean needsToLookupSuggestions() {
-        return mAutoCorrectionEnabledPerUserSettings || isSuggestionsEnabledPerUserSettings();
+        return (mInputAttributes.mShouldShowSuggestions || mOverrideShowingSuggestions)
+                && (mAutoCorrectionEnabledPerUserSettings || isSuggestionsEnabledPerUserSettings());
     }
 
     public boolean isSuggestionsEnabledPerUserSettings() {