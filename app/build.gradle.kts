4e38d86f168529adb276cb6ed0e1e864fdab8c3a
Turtlecute33 <95269376+Turtlecute33@users.noreply.github.com>
2026-04-26 12:38:39 +0200

    Prepare WisprBoard 6.0.9 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "helium314.keyboard.wisprboard"
         minSdk = 21
         targetSdk = 35
-        versionCode = 6008
-        versionName = "6.0.8"
+        versionCode = 6009
+        versionName = "6.0.9"
         buildConfigField("boolean", "ALLOW_USER_SUPPLIED_JNI", "false")
         buildConfigField("boolean", "ENABLE_GESTURE_DATA_GATHERING", "false")
         manifestPlaceholders["gestureDataProviderEnabled"] = "false"
