4cba8bd409f751774c5e68e5a590741ec3878769
Tobias Leinss <7684178+leinss@users.noreply.github.com>
2026-06-12 15:22:49 +0200

    chore: bump version to 6.8.3 with changelog (#4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "xyz.leinss.TobiBoard"
         minSdk = 21
         targetSdk = 35
-        versionCode = 6802
-        versionName = "6.8.2"
+        versionCode = 6803
+        versionName = "6.8.3"
         buildConfigField("boolean", "ALLOW_USER_SUPPLIED_JNI", "false")
         buildConfigField("boolean", "ENABLE_GESTURE_DATA_GATHERING", "false")
         manifestPlaceholders["gestureDataProviderEnabled"] = "false"
